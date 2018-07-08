1c6d394f712d49c91e2d649e68851952ebbe05d8
cdswyda <cdswyda@hotmail.com>
2018-07-08 23:43:34 +0800

    添加nginx 映射支持

--- app.js
+++ app.js
@@ -11,14 +11,20 @@ app.set("views", path.join(__dirname, "tpl"))
 app.set('view engine', 'ejs')
 
 app.get('/index.html', function (req, res) {
-    // console.log(req.headers)
+    console.log(req.headers)
     // res.send(JSON.stringify(req.headers))
     // res.send(util.inspect(req.headers,{depth:null}))
-    res.render("demo", {
+    let data = {
         links: ['/css/t1.css'],
         jss: ['/js/t1.js'],
         script: 'alert(\'t1\')'
-    })
+    }
+    data.prefix = req.headers.isexternal;
+    // if(req.headers.isexternal) {
+    //     data.links[0] = req.headers.isexternal + data.links[0]
+    //     data.jss[0] = req.headers.isexternal + data.jss[0]
+    // }
+    res.render("demo", data)
 })
 
 const server = app.listen('1120', function () {
